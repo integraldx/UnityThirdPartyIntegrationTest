32960fbd1948baa9746404703e40a255683573cc
integraldx <integraldx@integraldx.me>
2022-08-10 10:31:10 +0900

    Add required assets

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+    
+    private IEnumerator Coroutine()
+    {
+        while (Application.isPlaying)
+        {
+            Debug.Log("Hello World!");
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
