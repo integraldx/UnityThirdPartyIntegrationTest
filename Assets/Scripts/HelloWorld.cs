d07d018f479573894c9cc4626e655a09c66e0407
integraldx <integraldx@integradx.me>
2022-08-22 15:12:03 +0900

    Add assembly system

--- Assets/Scripts/HelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class HelloWorld : MonoBehaviour
-{
-    private void OnEnable()
-    {
-        Debug.Log("Hello World!");
-    }
-}
---
+++ Assets/Scripts/Main/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ThirdPartyTest.Main
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
