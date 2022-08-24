a12e322b8f17649e64d7307a8d0bd3ab192015c1
integraldx <integraldx@integraldx.me>
2022-08-24 18:07:16 +0900

    Add unity credentials export

--- .ci/set_envs_interactive.sh
+++ .ci/set_envs_interactive.sh
@@ -1,9 +1,25 @@
 #/bin/zsh
 
-echo "Hello World!!"
+echo "====== Set env script for local build of unity project ======"
 
 read "?Unity hub install base path (/Applications/Unity/Hub/Editor): " unity_hub_base_path
 unity_hub_base_path="${${unity_hub_base_path:-"/Applications/Unity/Hub/Editor"}%/}"
 echo $unity_hub_base_path
 
 export UNITY_HUB_BASE_PATH=$unity_hub_base_path
+
+read "?Unity username: " unity_username
+echo $unity_username
+
+export UNITY_USERNAME=$unity_username
+
+read -sr "?Unity password: " unity_password
+
+export UNITY_PASSWORD=$unity_password
+
+read "?Unity serial: " unity_serial
+echo $unity_serial
+
+export UNITY_SERIAL=$unity_serial
+
+echo "====== Export complete! ======"
